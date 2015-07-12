cd19dc83be2d082a45c1d5fcf0e2099d7751e3e6
s0_ma <tatsuya.soma@gmail.com>
2015-07-12 19:42:37 +0900

    modify ModalLayerDecorator.

--- View/ModalLayerDecorator.cpp
+++ View/ModalLayerDecorator.cpp
@@ -13,8 +13,12 @@
 void ModalLayerDecorator::makeModal(cocos2d::Node* node){
     auto listener = cocos2d::EventListenerTouchOneByOne::create();
     listener->setSwallowTouches(true);
-    listener->onTouchBegan = [](cocos2d::Touch *touch,cocos2d::Event*event)->bool{
-        return true;
+    listener->onTouchBegan = [node](cocos2d::Touch *touch,cocos2d::Event*event)->bool{
+        if(node->isVisible()){
+            return true;
+        }else{
+            return false;
+        }
     };
     auto dip = node->getEventDispatcher();
     dip->addEventListenerWithSceneGraphPriority(listener, node);
