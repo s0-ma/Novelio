50897b2632857f3cacca0d8379511221bd6c703f
s0_ma <tatsuya.soma@gmail.com>
2015-09-12 17:04:36 +0900

    remove unused files.

--- novelio_lua.back/api/__class_type_info.lua
+++
@@ -1,5 +0,0 @@
-
---------------------------------
--- @module __class_type_info
--- @parent_module nv
-
--- novelio_lua.back/novelio.cpp
+++
@@ -1,6 +0,0 @@
-#include "novelio.hpp"
-#include "CommandExecutor.h"
-#include "tolua_fix.h"
-#include "LuaBasicConversions.h"
-
-
